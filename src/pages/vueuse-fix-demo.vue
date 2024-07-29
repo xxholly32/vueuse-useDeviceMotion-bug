6be4a66a439e56872bcc6d2d61d1cb95532c6f72
xiangxiao <xxholly32@163.com>
2024-07-29 11:36:20 +0800

    fix: add demo and fix-demo

---
+++ src/pages/vueuse-fix-demo.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue'
+import { useDeviceMotion } from '@vueuse/core'
+
+const motion = reactive(useDeviceMotion())
+const text = computed(() => JSON.stringify({
+  acceleration: {
+    x: motion.acceleration.x,
+    y: motion.acceleration.y,
+    z: motion.acceleration.z,
+  },
+  accelerationIncludingGravity: {
+    x: motion.accelerationIncludingGravity.x,
+    y: motion.accelerationIncludingGravity.y,
+    z: motion.accelerationIncludingGravity.z,
+  },
+  rotationRate: {
+    alpha: motion.rotationRate.alpha,
+    beta: motion.rotationRate.beta,
+    gamma: motion.rotationRate.gamma,
+  },
+  interval: motion.interval,
+}, null, 2))
+// const text = computed(() => JSON.stringify(motion, null, 2))
+</script>
+
+<template>
+  <h1 class="mb-2">
+    Device Motion:
+  </h1>
+  <pre lang="json">{{ text }}</pre>
+</template>
