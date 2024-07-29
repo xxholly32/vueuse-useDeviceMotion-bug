1ccb7b858d0aac758e53380da942f735e82c3189
xiangxiao <xxholly32@163.com>
2024-07-29 10:32:54 +0800

    refactor: remove useless files

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,7 +7,7 @@
     <a
       i-carbon-logo-github icon-btn
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/xxholly32/vueuse-useDeviceMotion-bug"
       target="_blank"
       title="GitHub"
     />
